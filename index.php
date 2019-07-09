1c1658cf598f2cbb49314f7ff097e428eef842d9
Lorenzo Dalrio <lorenzo.dalrio@eng.it>
2019-07-09 09:24:32 +0200

    Fix

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    print("
+print("
     <!DOCTYPE html>
     <html>
     <head>
@@ -13,19 +13,18 @@
     <body>
     <p>
     <h2>
-        Hi! I am $_SERVER['SERVER_NAME'] and I am a container.
+    Hi\! I am \"$_SERVER['SERVER_NAME']\" and I am a container.
     </h2>
     </p>
     <p>
     <h3>
-        My IP address is $_SERVER['SERVER_ADDR'], you invoked me at $_SERVER['REQUEST_TIME']<br>
-        using this browser:\n$_SERVER['HTTP_USER_AGENT']
+    My IP address is $_SERVER['SERVER_ADDR'], you invoked me at $_SERVER['REQUEST_TIME']<br>
+    using this browser:\n$_SERVER['HTTP_USER_AGENT']
     </h3>
     </p>
     <p style='text-align:center;'>
     <em>Powered by <img src='assets/image001.png'></em>
     </p>
     </body>
-    </html>
-");
+    </html>");
 ?>
