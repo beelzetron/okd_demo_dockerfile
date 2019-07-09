1c944350c50eba68fe7754f5f58324bc749d528b
Lorenzo Dalrio <lorenzo.dalrio@eng.it>
2019-07-09 10:15:52 +0200

    Working version.

--- index.php
+++ index.php
@@ -1,30 +1,41 @@
 <?php
-print("
+date_default_timezone_set('Europe/Rome');
+$current_date = date('d/m/Y H:i:s');
+$str = "
     <!DOCTYPE html>
     <html>
     <head>
     <title>Hello!</title>
     <style>
     * {
+        text-align: center;
         font-family: Verdana, Geneva, sans-serif;
     }
     </style>
     </head>
     <body>
     <p>
+    <h1>
+    Hello!
+    </h1>
+    </p>
+    <p>
     <h2>
-    Hi\! I am \"$_SERVER['SERVER_NAME']\" and I am a container.
+    I am " . $_SERVER['SERVER_NAME'] . " and I am a container.
     </h2>
     </p>
     <p>
     <h3>
-    My IP address is $_SERVER['SERVER_ADDR'], you invoked me at $_SERVER['REQUEST_TIME']<br>
-    using this browser:\n$_SERVER['HTTP_USER_AGENT']
+    My IP address is " . $_SERVER['SERVER_ADDR'] . ", you invoked me at " . $current_date . "<br>
+    using this browser:\n" . $_SERVER['HTTP_USER_AGENT'] . "
     </h3>
     </p>
-    <p style='text-align:center;'>
-    <em>Powered by <img src='assets/image001.png'></em>
+    <p>
+    <em>Powered by<br>
+    <img src='assets/image001.png'></em>
     </p>
     </body>
-    </html>");
+    </html>";
+
+print($str);
 ?>
