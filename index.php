4cb20fde95c64d18f12dae37e527f2030fb70b01
Lorenzo Dalrio <lorenzo.dalrio@eng.it>
2019-07-09 09:09:13 +0200

    New customized page.

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-<?php phpinfo(); ?>
+<?php
+    print("
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Hello!</title>
+    <style>
+    * {
+        font-family: Verdana, Geneva, sans-serif;
+    }
+    <meta charset="UTF-8">
+    </head>
+    <body>
+    <p>
+    <h2>
+        Hi! I am $_SERVER['SERVER_NAME'] and I am a container.
+    </h2>
+    </p>
+    <p>
+    <h3>
+        My IP address is $_SERVER['SERVER_ADDR'], you invoked me at $_SERVER['REQUEST_TIME']<br>
+        using this browser:\n$_SERVER['HTTP_USER_AGENT']
+    </h3>
+    </p>
+    <p style="text-align:center;">
+    <em>Powered by <img src="assets/image001.png"></em>
+    </p>
+    </body>
+    </html>
+")
+?>
