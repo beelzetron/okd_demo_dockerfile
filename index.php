d3d1fe9c2316d85ba9a84b7b9f8f774b9bf3f8f4
Lorenzo Dalrio <lorenzo.dalrio@eng.it>
2019-07-09 10:47:33 +0200

    Style changes (2).

--- index.php
+++ index.php
@@ -3,43 +3,38 @@ date_default_timezone_set('Europe/Rome');
 $current_date = date('d/m/Y H:i:s');
 $hostname = gethostname();
 $str = "
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <title>Hello!</title>
-    <style>
-    * {
-        text-align: center;
-        font-family: Verdana, Geneva, sans-serif;
-    }
-    </style>
-    </head>
-    <body>
-    <p>
-    <h1>
-    Hello!
-    </h1>
-    </p>
-    <p>
-    <h2>
-    I am " . $hostname . " and I am a container.
-    </h2>
-    </p>
-    <p>
-    <h3>
-    My IP address is " . $_SERVER['SERVER_ADDR'] . ", you invoked me at " . $current_date . "<br>
-    using this browser:</p>" . 
-    "<p style='font-family:courier;'>" . $_SERVER['HTTP_USER_AGENT'] . "</p>" . "
-    </h3>
-    <p>
-    <em>Powered by<br>
-    <img src='image001.png'></em>
-    </p>
-    <p>
-    <em>version 1.0</em>
-    </p>
-    </body>
-    </html>";
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hello!</title>
+<style>
+* {
+    text-align: center;
+    font-family: Verdana, Geneva, sans-serif;
+}
+</style>
+</head>
+<body>
+<h1>
+<p>Hello!</p>
+</h1>
+<h2>
+<p>I am</p>
+<p style='font-family:courier;'> " . $hostname . "</p>
+<p> and I am a container.</p>
+</h2>
+<h4>
+<p>My IP address is</p>
+<p style='font-family:courier;'>" . $_SERVER['SERVER_ADDR'] . "</p>
+<p>you invoked me at " . $current_date . "</p>
+<p>using this browser:</p>" . "
+<p style='font-family:courier;'>" . $_SERVER['HTTP_USER_AGENT'] . "</p>
+</h4>
+<p><em>Powered by<br>
+<img src='image001.png'></em></p>
+<p><em>version 1.0</em></p>
+</body>
+</html>";
 
 print($str);
 ?>
